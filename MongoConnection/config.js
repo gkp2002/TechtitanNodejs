25701f15507bc9825588f380397c970fe10e0a0f
gkp2002 <gajanank015@gmail.com>
2024-05-07 17:44:58 +0530

    install mongodb

--- MongoConnection/config.js
+++ MongoConnection/config.js
@@ -1,3 +1,4 @@
 const mongoose = require('mongoose');
+require("dotenv").config()
  // mongoose.connect("mongodb://localhost:27017/Techtitans");
-  mongoose.connect('mongodb+srv://gajanantraders522:7tr7reJRuutXTwNb@cluster0.krchmkq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+  mongoose.connect(process.env.MONGODB_URL)
